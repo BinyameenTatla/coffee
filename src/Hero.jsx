63d66fe2ef5ed3c10dfd4406e952cc2c0e7eafd4
BinyameenTatla <binyameentatla204@gmail.com>
2025-02-24 22:53:00 -0800

    image

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
             <h2><p>welcome!</p>
                <b className="red"> COFFEE FACTORY</b>
                 <hr></hr>
-                <p className="p">Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br></br> Pariatur quidem incidunt aperiam dicta sint.<br></br></p>
+                <p className="p">Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br></br> Pariatur quidem incidunt aperiam dicta sint.<br></br></p><br></br>
                 <button className="button">Shop Here</button>
             </h2>
 
